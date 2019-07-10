6cf5fd3a02a11499d468b3d7fe93dc3ef09a6096
Djavan Munroe <djavanm24@gmail.com>
2019-07-10 15:38:11 -0600

    Add linting to scripts

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,3 @@
-console.log("Hello World");
-
 const globalRepo = new UserRepository(userData);
 let currentUser = new User(globalRepo.returnUser(getRandomNumber()));
 let currentHydration = new Hydration(hydrationData, currentUser.id);
@@ -71,9 +69,9 @@ $(document).ready(function() {
 function getRandomNumber() {
   let randNum = (Math.random() * 50) + 1;
   randNum = Math.floor(randNum);
-  console.log(randNum);
   return randNum;
 }
+
 function friendsStepChallenge() {
     let friends = currentUser.friends;
     friends.push(currentUser.id);
@@ -85,45 +83,44 @@ function friendsStepChallenge() {
             'weeklySteps': friend.returnWeeklySteps(today),
             'name': globalRepo.returnUser(friend.id).name
         }
-    })
+    });
     friends.sort((a,b) => {
         return b.weeklySteps - a.weeklySteps;
-    })
+    });
     return generateFriendsElements(friends);
-} 
-
-function generateFriendsElements(users) {
-    let friendsList = '<ol class="main__section--friends--challenge">'
-    users.forEach(user => {
-        friendsList += `<li><p>${user.name}: </p><p>${user.weeklySteps} steps</p></li>`
-    })
-    friendsList += '</ol>'
-    console.log(friendsList)
-    return friendsList
 };
 
+function generateFriendsElements(users) {
+  let friendsList = '<ol class="main__section--friends--challenge">'
+  users.forEach(user => {
+    friendsList += `<li><p>${user.name}: </p><p>${user.weeklySteps} steps</p></li>`
+  })
+  friendsList += '</ol>'
+  return friendsList;
+}
+
 function setDateString() {
-  let date = Date().split(' ')
-  let returnDate = [2,0,1,9,'/',0];
-  if(date[1] === 'Jul') {
+  let date = Date().split(' ');
+  let returnDate = [2, 0, 1, 9, '/', 0];
+  if (date[1] === 'Jul') {
     returnDate.push(7);
     returnDate.push('/');
     returnDate.push(date[2]);
     return returnDate.join('');
-  } else if(date[1] === 'Aug') {
-    returnDate.push(8)
+  } else if (date[1] === 'Aug') {
+    returnDate.push(8);
     returnDate.push('/');
     returnDate.push(date[2]);
     return returnDate.join('');
-  } else if(date[1] === 'Sep') {
-    returnDate.push(9)
+  } else if (date[1] === 'Sep') {
+    returnDate.push(9);
     returnDate.push('/');
     returnDate.push(date[2]);
     return returnDate.join('');
   } else {
-    return '2019/09/21'
+    return '2019/09/21';
     }
-  }
+  };
 
 // Chart  Section //
 
@@ -133,36 +130,32 @@ Chart.defaults.scale.ticks.beginAtZero = true;
 
 let hydrationWeekData = currentHydration.returnWeekIntake(today);
 let hydrationDays = hydrationWeekData.reduce((acc, day) => {
-    let today = day;
-    let newDate = today.date.split('/').filter(index => index.length !== 4).join('/');
+  let today = day;
+  let newDate = today.date.split('/').filter(index => index.length !== 4).join('/');
     acc.push(newDate);
     return acc;
   }, []);
 
-const ctx = $('#hydration-chart')
+const ctx = $('#hydration-chart');
 const chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-    // The data for our dataset
-    data: {
-        labels: [`${hydrationDays[0]}`, `${hydrationDays[1]}`, `${hydrationDays[2]}`, `${hydrationDays[3]}`, `${hydrationDays[4]}`, `${hydrationDays[5]}`, `${hydrationDays[6]}`],
-        datasets: [{
-            label: 'My Last Week of Hydration in Ounces',
-            backgroundColor: '#0d6aff',
-            borderColor: 'black',
-            borderWidth: 2,
-            data: [`${hydrationWeekData[0].numOunces}`, `${hydrationWeekData[1].numOunces}`, `${hydrationWeekData[2].numOunces}`, `${hydrationWeekData[3].numOunces}`, `${hydrationWeekData[4].numOunces}`, `${hydrationWeekData[5].numOunces}`, `${hydrationWeekData[6].numOunces}`]
+  type: 'line',
+  data: {
+    labels: [`${hydrationDays[0]}`, `${hydrationDays[1]}`, `${hydrationDays[2]}`, `${hydrationDays[3]}`, `${hydrationDays[4]}`, `${hydrationDays[5]}`, `${hydrationDays[6]}`],
+      datasets: [{
+        label: 'My Last Week of Hydration in Ounces',
+        backgroundColor: '#0d6aff',
+        borderColor: 'black',
+        borderWidth: 2,
+        data: [`${hydrationWeekData[0].numOunces}`, `${hydrationWeekData[1].numOunces}`, `${hydrationWeekData[2].numOunces}`, `${hydrationWeekData[3].numOunces}`, `${hydrationWeekData[4].numOunces}`, `${hydrationWeekData[5].numOunces}`, `${hydrationWeekData[6].numOunces}`]
         }]
     },
-
-    // Configuration options go here
     options: {
-        title: {
-            display: true,
-            text: 'Water Intake This Week'
-        },
+      title: {
+        display: true,
+        text: 'Water Intake This Week'
+      },
         legend: {
-            display: false
+          display: false
         }
     }
 
@@ -170,68 +163,58 @@ const chart = new Chart(ctx, {
 
 let sleepWeek = currentSleep.returnWeekHours(today);
 let sleepDays = sleepWeek.reduce((acc, day) => {
-    let today = day;
-    let newDate = today.date.split('/').filter(index => index.length !== 4).join('/');
+  let today = day;
+  let newDate = today.date.split('/').filter(index => index.length !== 4).join('/');
     acc.push(newDate);
     return acc;
   }, []);
 
 const ctx2 = $('#sleep-hours-chart')
 const chart2 = new Chart(ctx2, {
-    // The type of chart we want to create
-    type: 'horizontalBar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-    // The data for our dataset
-    data: {
-        labels: [`${sleepDays[0]}`, `${sleepDays[1]}`, `${sleepDays[2]}`, `${sleepDays[3]}`, `${sleepDays[4]}`, `${sleepDays[5]}`, `${sleepDays[6]}`],
-        datasets: [{
-            label: 'Hours Slept',
-            backgroundColor:  '#7924b3',
-            borderColor: '#000000',
-            borderWidth: 1,
-            data: [`${sleepWeek[0].hoursSlept}`, `${sleepWeek[1].hoursSlept}`, `${sleepWeek[2].hoursSlept}`, `${sleepWeek[3].hoursSlept}`, `${sleepWeek[4].hoursSlept}`, `${sleepWeek[5].hoursSlept}`, `${sleepWeek[6].hoursSlept}`]
-        }]
+  type: 'horizontalBar',
+  data: {
+    labels: [`${sleepDays[0]}`, `${sleepDays[1]}`, `${sleepDays[2]}`, `${sleepDays[3]}`, `${sleepDays[4]}`, `${sleepDays[5]}`, `${sleepDays[6]}`],
+    datasets: [{
+      label: 'Hours Slept',
+      backgroundColor:  '#7924b3',
+      borderColor: '#000000',
+      borderWidth: 1,
+      data: [`${sleepWeek[0].hoursSlept}`, `${sleepWeek[1].hoursSlept}`, `${sleepWeek[2].hoursSlept}`, `${sleepWeek[3].hoursSlept}`, `${sleepWeek[4].hoursSlept}`, `${sleepWeek[5].hoursSlept}`, `${sleepWeek[6].hoursSlept}`]
+    }]
     },
-
-    // Configuration options go here
     options: {
-        title: {
-            display: true,
-            text: 'Hours Slept this Week'
-        },
+      title: {
+        display: true,
+        text: 'Hours Slept this Week'
+      },
         legend: {
-            display: false
+          display: false
         }
     }
-
 });
 
 const ctx5 = $('#sleep-quality-chart')
 const chart5 = new Chart(ctx5, {
-    // The type of chart we want to create
-    type: 'horizontalBar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-    // The data for our dataset
-    data: {
-        labels: [`${sleepDays[0]}`, `${sleepDays[1]}`, `${sleepDays[2]}`, `${sleepDays[3]}`, `${sleepDays[4]}`, `${sleepDays[5]}`, `${sleepDays[6]}`],
-        datasets: [{
-            label: 'Sleep Quality',
-            backgroundColor:  '#7924b3',
-            borderColor: '#000000',
-            borderWidth: 1,
-            data: [`${sleepWeek[0].sleepQuality}`, `${sleepWeek[1].sleepQuality}`, `${sleepWeek[2].sleepQuality}`, `${sleepWeek[3].sleepQuality}`, `${sleepWeek[4].sleepQuality}`, `${sleepWeek[5].sleepQuality}`, `${sleepWeek[6].sleepQuality}`]
-        }]
+  type: 'horizontalBar',
+  data: {
+    labels: [`${sleepDays[0]}`, `${sleepDays[1]}`, `${sleepDays[2]}`, `${sleepDays[3]}`, `${sleepDays[4]}`,`${sleepDays[5]}`, `${sleepDays[6]}`],
+      datasets: [{
+        label: 'Sleep Quality',
+        backgroundColor:  '#7924b3',
+        borderColor: '#000000',
+        borderWidth: 1,
+        data: [`${sleepWeek[0].sleepQuality}`, `${sleepWeek[1].sleepQuality}`, `${sleepWeek[2].sleepQuality}`, `${sleepWeek[3].sleepQuality}`, `${sleepWeek[4].sleepQuality}`, `${sleepWeek[5].sleepQuality}`, `${sleepWeek[6].sleepQuality}`]
+      }]
     },
-
-    // Configuration options go here
     options: {
-        title: {
-            display: true,
-            text: 'Sleep Quality this Week'
-        },
-        legend: {
-            display: false
-        }
+      title: {
+        display: true,
+        text: 'Sleep Quality this Week'
+      },
+      legend: {
+        display: false
+      }
     }
-
 });
 
 let activityWeek = activity.returnWeekInfo(today);
@@ -244,118 +227,105 @@ let activityDays = activityWeek.reduce((acc, day) => {
 
 const ctx7 = $('#activity-minutes-chart');
 const chart7 = new Chart(ctx7, {
-    // The type of chart we want to create
-    type: 'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-    // The data for our dataset
-    data: {
-      labels: [`${activityDays[0]}`, `${activityDays[1]}`, `${activityDays[2]}`, `${activityDays[3]}`, `${activityDays[4]}`, `${activityDays[5]}`, `${activityDays[6]}`],
-      datasets: [{
-          label: 'Activity Per Day',
-          backgroundColor:  '#FFE400',
-          borderColor: '#000000',
-          borderWidth: 1,
-          data: [`${activityWeek[0].minutesActive}`, `${activityWeek[1].minutesActive}`, `${activityWeek[2].minutesActive}`, `${activityWeek[3].minutesActive}`, `${activityWeek[4].minutesActive}`, `${activityWeek[5].minutesActive}`, `${activityWeek[6].minutesActive}`]
-      }]
+  type: 'line',
+  data: {
+    labels: [`${activityDays[0]}`, `${activityDays[1]}`, `${activityDays[2]}`, `${activityDays[3]}`, `${activityDays[4]}`, `${activityDays[5]}`, `${activityDays[6]}`],
+    datasets: [{
+      label: 'Activity Per Day',
+      backgroundColor:  '#FFE400',
+      borderColor: '#000000',
+      borderWidth: 1,
+      data: [`${activityWeek[0].minutesActive}`, `${activityWeek[1].minutesActive}`, `${activityWeek[2].minutesActive}`, `${activityWeek[3].minutesActive}`, `${activityWeek[4].minutesActive}`, `${activityWeek[5].minutesActive}`, `${activityWeek[6].minutesActive}`]
+    }]
   },
-
-    // Configuration options go here
     options: {
-        scales: {
-            yAxes: [{
-                barPercentage: 0.5,
-                barThickness: 1000,
-                maxBarThickness: 8,
-                minBarLength: 20,
-                gridLines: {
-                    offsetGridLines: true
-                }
-            }]
-        },
-        title: {
-            display: true,
-            text: 'Active Minutes this Week'
-        },
-        legend: {
-            display: false
-        }
+      scales: {
+        yAxes: [{
+          barPercentage: 0.5,
+          barThickness: 1000,
+          maxBarThickness: 8,
+          minBarLength: 20,
+          gridLines: {
+            offsetGridLines: true
+          }
+        }]
+      },
+      title: {
+        display: true,
+        text: 'Active Minutes this Week'
+      },
+      legend: {
+        display: false
+      }
     }
 });
 
 const ctx8 = $('#activity-stairs-chart');
 const chart8 = new Chart(ctx8, {
-    // The type of chart we want to create
-    type: 'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-    // The data for our dataset
-    data: {
-      labels: [`${activityDays[0]}`, `${activityDays[1]}`, `${activityDays[2]}`, `${activityDays[3]}`, `${activityDays[4]}`, `${activityDays[5]}`, `${activityDays[6]}`],
-      datasets: [{
-          label: 'Activity Per Day',
-          backgroundColor:  '#FFE400',
-          borderColor: '#000000',
-          borderWidth: 1,
-          data: [`${activityWeek[0].flightsOfStairs}`, `${activityWeek[1].flightsOfStairs}`, `${activityWeek[2].flightsOfStairs}`, `${activityWeek[3].flightsOfStairs}`, `${activityWeek[4].flightsOfStairs}`, `${activityWeek[5].flightsOfStairs}`, `${activityWeek[6].flightsOfStairs}`]
-      }]
-  },
-
-    // Configuration options go here
-    options: {
-        scales: {
-            yAxes: [{
-                barPercentage: 0.5,
-                barThickness: 1000,
-                maxBarThickness: 8,
-                minBarLength: 20,
-                gridLines: {
-                    offsetGridLines: true
-                }
-            }]
-        },
-        title: {
-            display: true,
-            text: 'Flights of Stairs Climbed this Week'
-        },
-        legend: {
-            display: false
+  type: 'line',
+  data: {
+    labels: [`${activityDays[0]}`, `${activityDays[1]}`, `${activityDays[2]}`, `${activityDays[3]}`, `${activityDays[4]}`, `${activityDays[5]}`, `${activityDays[6]}`],
+    datasets: [{
+      label: 'Activity Per Day',
+      backgroundColor:  '#FFE400',
+      borderColor: '#000000',
+      borderWidth: 1,
+      data: [`${activityWeek[0].flightsOfStairs}`, `${activityWeek[1].flightsOfStairs}`, `${activityWeek[2].flightsOfStairs}`, `${activityWeek[3].flightsOfStairs}`, `${activityWeek[4].flightsOfStairs}`, `${activityWeek[5].flightsOfStairs}`, `${activityWeek[6].flightsOfStairs}`]
+    }]
+},
+  options: {
+    scales: {
+      yAxes: [{
+        barPercentage: 0.5,
+        barThickness: 1000,
+        maxBarThickness: 8,
+        minBarLength: 20,
+        gridLines: {
+            offsetGridLines: true
         }
+      }]
+    },
+    title: {
+      display: true,
+      text: 'Flights of Stairs Climbed this Week'
+    },
+    legend: {
+      display: false
     }
+  }
 });
-
 
 const ctx9 = $('#activity-steps-chart');
 const chart9 = new Chart(ctx9, {
-    // The type of chart we want to create
-    type: 'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-    // The data for our dataset
-    data: {
-      labels: [`${activityDays[0]}`, `${activityDays[1]}`, `${activityDays[2]}`, `${activityDays[3]}`, `${activityDays[4]}`, `${activityDays[5]}`, `${activityDays[6]}`],
-      datasets: [{
-          label: 'Activity Per Day',
-          backgroundColor:  '#FFE400',
-          borderColor: '#000000',
-          borderWidth: 1,
-          data: [`${activityWeek[0].numSteps}`, `${activityWeek[1].numSteps}`, `${activityWeek[2].numSteps}`, `${activityWeek[3].numSteps}`, `${activityWeek[4].numSteps}`, `${activityWeek[5].numSteps}`, `${activityWeek[6].numSteps}`]
-      }]
+  type: 'line',
+  data: {
+    labels: [`${activityDays[0]}`, `${activityDays[1]}`, `${activityDays[2]}`, `${activityDays[3]}`, `${activityDays[4]}`, `${activityDays[5]}`, `${activityDays[6]}`],
+    datasets: [{
+      label: 'Activity Per Day',
+      backgroundColor:  '#FFE400',
+      borderColor: '#000000',
+      borderWidth: 1,
+      data: [`${activityWeek[0].numSteps}`, `${activityWeek[1].numSteps}`, `${activityWeek[2].numSteps}`, `${activityWeek[3].numSteps}`, `${activityWeek[4].numSteps}`, `${activityWeek[5].numSteps}`, `${activityWeek[6].numSteps}`]
+    }]
   },
-
-    // Configuration options go here
-    options: {
-        scales: {
-            yAxes: [{
-                barPercentage: 0.5,
-                barThickness: 1000,
-                maxBarThickness: 8,
-                minBarLength: 20,
-                gridLines: {
-                    offsetGridLines: true
-                }
-            }]
-        },
-        title: {
-            display: true,
-            text: 'Flights of Stairs Climbed this Week'
-        },
-        legend: {
-            display: false
+  options: {
+    scales: {
+      yAxes: [{
+        barPercentage: 0.5,
+        barThickness: 1000,
+        maxBarThickness: 8,
+        minBarLength: 20,
+        gridLines: {
+            offsetGridLines: true
         }
+      }]
+    },
+    title: {
+      display: true,
+      text: 'Flights of Stairs Climbed this Week'
+    },
+    legend: {
+      display: false
     }
+  }
 });
